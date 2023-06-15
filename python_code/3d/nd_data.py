900a462066f2e8d7c8522011691706de40772ac3
senwa105 <senwa105@gmail.com>
2023-06-14 23:17:52 -0400

    add nd_data

---
+++ python_code/3d/nd_data.py
@@ -0,0 +1,26 @@
+# from _ import g
+
+import numpy as np
+
+def nd_data(
+        l_f, 
+        c_f,
+        h_f,
+        l_r,
+        c_r,
+        h_r,
+        phiT_,
+        phiB_,
+        a_,
+        beta_,
+        delta_,
+        gMax_,
+        U_,
+        xb_f,
+        Xc_f,
+        Xb_r,
+        Xc_r,
+        b_f,
+        b_r
+):
+    """Non-dimensionalize the input data"""
