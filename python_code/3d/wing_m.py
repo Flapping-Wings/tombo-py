285330116043f2bdfdc24a627edfebf328d0c3ce
senwa105 <senwa105@gmail.com>
2023-06-20 13:38:09 -0400

    add documentation

--- python_code/3d/wing_m.py
+++ python_code/3d/wing_m.py
@@ -1,4 +1,39 @@
 def wing_m(mpath, t, rt, tau, e, gMax, p, rtOff, phiT, phiB):
     """
-    Calculate airfoil translational and rotational parameters
+    Calculate airfoil translational and rotational parameters.
+    All input parameters are nondimensional.
+
+    Parameters
+    ----------
+    mpath: int
+        Motion path parameter
+    t: float
+        Time
+    rt: float
+        Ratio of T_[0] / T[i]
+    tau: float
+        Phase shift
+    e: float
+        Stroke difference
+    gMax: float
+        Max rotation angle
+    p: float
+        Rotation speed parameter
+    rtOff: float
+        Rotation timing offset
+    phiT: float
+        Top stroke angle
+    phiB: float
+        Bottom stroke angle
+
+    Returns
+    -------
+    phi: float
+        Rolling angle
+    theta: float
+        Rotation angle
+    dph: float
+        Derivative of phi with respect to time (dphi / dt)
+    dth: float
+        Derivative of theta with respect to time (dtheta) / dt)
     """
