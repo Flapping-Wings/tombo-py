962b10afd90857e8a845a57a207d7ea1a88ea8b4
senwa105 <senwa105@gmail.com>
2023-06-21 14:40:15 -0400

    implement setup phase

--- python_code/3d/tombo.py
+++ python_code/3d/tombo.py
@@ -1,8 +1,87 @@
 import numpy as np
 from globals import g
+from nd_data import nd_data
+from wing_total import wing_total
 
 def tombo():
-    pass
+    # SETUP
+    # -----
+
+    # g.xb_f, g.nxb_f, g.nb_f, g.xc_f, g.nxc_f, g.nc_f, g.l_f, g.c_f, g.h_f,
+    # g.xb_r, g.nxb_r, g.nb_r, g.xc_r, g.nxc_r, g.nc_r, g.l_r, g.c_r, g.h_r = \
+    #     Wing()
+
+    l, c, h, phiT, phiB, a, beta, delta, gMax, U, \
+    xb_f, xc_f, xb_r, xc_r, b_f, b_r, e, d =      \
+        nd_data(g.l_f, g.c_f, g.h_f, g.l_r, g.c_r, g.h_r, 
+                g.phiT_, g.phiB_, g.a_, g.beta_, g.delta_, g.gMax_, g.U_, 
+                g.xb_f, g.xc_f, g.xb_r, g.xc_r, g.b_f, g.b_r)
+    
+    g.LCUT = 0.1 * h[0]  
+    
+    # Front right wing
+    xc_f, xb_f, xt_f, nxt_f, xC_f, nC_f = \
+        wing_total(xb_f, g.nxb_f, g.nb_f, xc_f, g.nxc_f, g.nc_f)
+    # Rear right wing
+    xc_r, xb_r, xt_r, nxt_r, xC_r, nC_r = \
+        wing_total(xb_r, g.nxb_r, g.nb_r, xc_r, g.nxc_r, g.nc_r)
+    
+    # Wake vortex magnitude array
+    GAMw_f = np.zeros((g.nwing, g.nxb_f))
+    GAMw_r = np.zeros((g.nwing, g.nxb_r))
+    # Total wake vortex number
+    nxw_f = 0; nxw_r = 0
+    # Wake vortex location array (after convection)
+    Xw_f = np.zeros((3, 4, g.nxb_f, g.nwing))
+    Xw_r = np.zeros((3, 4, g.nxb_r, g.nwing))
+    # Shed vortex location array 
+    Xs_f = np.zeros((3, 4, g.nxb_f, g.nwing))
+    Xs_r = np.zeros((3, 4, g.nxb_r, g.nwing))
+
+    if g.nstep > 3:
+        # Initialize the linear and angular impulse arrays
+        limpa_f = np.zeros((3, g.nstep, g.nwing))
+        limpa_r = np.zeros((3, g.nstep, g.nwing))
+        aimpa_f = np.zeros((3, g.nstep, g.nwing))
+        aimpa_r = np.zeros((3, g.nstep, g.nwing))
+        limpw_f = np.zeros((3, g.nstep, g.nwing))
+        limpw_r = np.zeros((3, g.nstep, g.nwing))
+        aimpw_f = np.zeros((3, g.nstep, g.nwing))
+        aimpw_r = np.zeros((3, g.nstep, g.nwing))
+
+    # Normal velocity on the wing due to the wing motion & wake vortices
+    Vnc_f  = np.zeros((g.nwing, nxt_f))
+    Vnc_r  = np.zeros((g.nwing, nxt_r))
+    Vncw_f = np.zeros((g.nwing, nxt_r))
+    Vncw_r = np.zeros((g.nwing, nxt_f))
+
+    # Sub-matrix for the non-penetration condition (self-terms)
+    MVNs_f = np.zeros((nxt_f, nxt_f, g.nwing))
+    MVNs_r = np.zeros((nxt_r, nxt_r, g.nwing))
+
+    # Velocity value matrices
+    VBW_f = np.zeros((3, 4, g.nxb_f, g.nwing))
+    VBW_r = np.zeros((3, 4, g.nxb_r, g.nwing))
+
+    # TODO: Document Xc_f/r
+    Xc_f = np.zeros((3, 4, g.nxc_f, 2))
+    Xc_r = np.zeros((3, 4, g.nxc_r, 2))
+    # Space-fixed system coords of border elements on the wing
+    Xb_f = np.zeros((3, 4, g.nxb_f, 2))
+    Xb_r = np.zeros((3, 4, g.nxb_r, 2))
+    # Global coords of total elements on the wing
+    Xt_f = np.zeros((3, 4, nxt_f, 2))
+    Xt_r = np.zeros((3, 4, nxt_r, 2))
+    # Global coords of the collocation points on the wing
+    XC_f = np.zeros((3, nxt_f, 2))
+    XC_r = np.zeros((3, nxt_r, 2))
+    # Unit normals at the collocation points on the wing
+    NC_f = np.zeros((3, nxt_f, 2))
+    NC_r = np.zeros((3, nxt_r, 2))
+
+
+    # TIME MARCH
+    # ----------
 
 
 def check_input():
