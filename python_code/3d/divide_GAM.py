8b21e7b089dfb750f8a0779b82d7a2147a6492fd
senwa105 <senwa105@gmail.com>
2023-06-26 15:17:03 -0400

    add func signature and documentation

---
+++ python_code/3d/divide_GAM.py
@@ -0,0 +1,19 @@
+import numpy as np
+from globals import g
+
+def divide_GAM(GAM, nxb):
+    """
+    Sort GAM into shed and bound groups
+
+    Parameters
+    ----------
+    GAM: ndarray[w, nxt]
+        TODO
+    nxb: Number of border elements
+
+    Returns
+    -------
+    GAMAb: ndarray[w, nxb]
+        Border elements (to be shed)
+    """
+    pass
