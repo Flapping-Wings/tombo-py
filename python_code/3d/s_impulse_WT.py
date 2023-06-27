7c47ff4223156d411a6a52a712a1a2fb80af1a2d
senwa105 <senwa105@gmail.com>
2023-06-27 14:45:22 -0400

    add TODO comment

--- python_code/3d/s_impulse_WT.py
+++ python_code/3d/s_impulse_WT.py
@@ -1,6 +1,7 @@
 import numpy as np
 from globals import g
 
+# TODO: Remove unused parameters beta, phi, theta
 def s_impulse_WT(istep, U, t, Xt, Xw, GAM, GAMAw, beta, phi, theta, a):
     """
     Calculate linear and angular impulses due to bound and wake 
