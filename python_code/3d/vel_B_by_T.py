d807583b95aa1e2a28aae874651e4ac41cebd47a
Ashrit Yarava <ashdragon95@gmail.com>
2023-06-29 15:01:27 -0400

    Create vel_B_by_T.py
    Completed function with proper checks done.

---
+++ python_code/3d/vel_B_by_T.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from globals import g
+
+
+def vel_B_by_T(cVBT, GAM, nXt):
+    s = np.shape(cVBT)
+    r = (s[0], s[1], s[2], g.nwing)
+    VBT = np.zeros(r)
+    dVBT = np.zeros(r)
+
+    for w in range(g.nwing):
+        for itelem in range(nXt):
+            dVBT[:, :, :, w] = cVBT[:, :, :, itelem, w] * GAM[w, itelem]
+            VBT[:, :, :, w] = VBT[:, :, :, w] + dVBT[:, :, :, w]
+
+    return VBT
+
+
+if __name__ == "__main__":
+    from scipy.io import loadmat
+
+    data = loadmat("data.mat")
+
+    cVBT = data['cVBT']
+    GAM = data['GAM']
+    nXt = data['nXt']
+    nXt = int(nXt)
+
+    VBT_og = data['VBT']
+    VBT = vel_B_by_T(cVBT, GAM, nXt)
+
+    print(np.alltrue(np.isclose(VBT_og, VBT)))
