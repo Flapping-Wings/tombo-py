0861eee3ee434ad57e93d3bb1944adfb8a7232df
senwa105 <senwa105@gmail.com>
2023-07-03 11:01:41 -0400

    implement plotting

--- python_code/3d/lrs_wing_NVs.py
+++ python_code/3d/lrs_wing_NVs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from globals import g
 
 def lrs_wing_NVs(m, iwing, xC, XC, NC, t, theta, phi, dph, dth, a, beta, U):
@@ -65,11 +66,46 @@ def lrs_wing_NVs(m, iwing, xC, XC, NC, t, theta, phi, dph, dth, a, beta, U):
     Vnc = vx * NC[0, :] + vy * NC[1, :] + vz * NC[2, :]
 
     if g.vplot:
-        plot_normal_vel()
+        plot_normal_vel(XC, Vnc, NC, m, iwing, t)
 
     return Vnc
 
 
 # TODO
-def plot_normal_vel():
-    pass
+def plot_normal_vel(XC, Vnc, NC, m, iwing, t):
+    # End points for the normal velocity vector
+    sf = 0.1    # Scale factor for the velocity plot
+    xaif = XC[0,:]
+    yaif = XC[1,:]
+    zaif = XC[2,:]
+    
+    xtip = xaif + sf * Vnc * NC[0,:]
+    ytip = yaif + sf * Vnc * NC[1,:]
+    ztip = zaif + sf * Vnc * NC[2,:]
+    
+    # Plot normal velocity vectors at collocation points     
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for i in range(len(xtip)):
+      ax.plot([xaif[i],xtip[i]], [yaif[i],ytip[i]], [zaif[i],ztip[i]])
+
+    ax.scatter(XC[0,:], XC[1,:], XC[2,:], marker='o')
+
+    ax.set_title('Normal velocity vectors at collocation points')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_box_aspect([1, 1, 1])
+    ax.axis('equal')
+
+    if m == 0:
+        if iwing == 0:
+            plt.savefig(g.folder + 'debug/Vairfoil_fr_' + f'{t:.4f}' + '.png')
+        else:
+            plt.savefig(g.folder + 'debug/Vairfoil_fl_' + f'{t:.4f}' + '.png')
+    else:
+        if iwing == 0:
+            plt.savefig(g.folder + 'debug/Vairfoil_rr_' + f'{t:.4f}' + '.png')
+        else:
+            plt.savefig(g.folder + 'debug/Vairfoil_rl_' + f'{t:.4f}' + '.png')
