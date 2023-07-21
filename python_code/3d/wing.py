95c3e841c690ad571d05654ede7415b0dabd9e96
senwa105 <senwa105@gmail.com>
2023-07-21 01:39:28 -0400

    delete wing function

--- python_code/3d/wing.py
+++ python_code/3d/wing.py
@@ -2,54 +2,6 @@ import numpy as np
 import globals as g
 import matplotlib.pyplot as plt
 
-def wing():
-    """
-    Initialize all Wing dimensions for the forward and rear wings to use later in the code.
-
-    Output (ALL DIMENSIONAL):
-
-    -------FORWARD WING--------
-    Xb_f[j, n, i]    : Border element coordinates
-    nXb_f            : # of border elements
-    Nb_f[j, i]       : Unit normal to the border elements
-    Xc_f[j, n, i]    : Center element coordinates
-    nXc_f            : # of center elements
-    Nc_f[j, i]       : Unit normal to the center elements
-    l_f              : Span
-    c_f              : Chord
-    h_f              : Border Height
-
-    
-    
-    ---------REAR WING---------
-    Xb_r[j, n, i]    : Border element coordinates
-    nXb_r            : # of border elements
-    Nb_r[j, i]       : Unit normal to the border elements
-    Xc_r[j, n, i]    : Center element coordinates
-    nXc_r            : # of center elements
-    Nc_r[j, i]       : Unit normal to the center elements
-    l_r              : Span
-    c_r              : Chord
-    h_r              : Border Height 
-    """
-
-    # Forward Wing Dimensions
-    hfactor_f = 0.1
-    wfactor_f = 3
-
-    Xb_f, nXb_f, Nb_f, Xc_f, nXc_f, Nc_f, l_f, c_f = symmetric_5_sided_mesh(1, 2, 2, 30, hfactor_f, wfactor_f)
-    h_f = c_f * hfactor_f
-
-    # Rear Wing Dimensions
-    hfactor_r = 0.1
-    wfactor_r = 3
-
-    Xb_r, nXb_r, Nb_r, Xc_r, nXc_r, Nc_r, l_r, c_r = symmetric_5_sided_mesh(2, 1, 1, 30, hfactor_r, wfactor_r)
-    h_r = c_r * hfactor_r
-
-    return Xb_f, nXb_f, Nb_f, Xc_f, nXc_f, Nc_f, l_f, c_f, h_f, Xb_r, nXb_r, Nb_r, Xc_r, nXc_r, Nc_r, l_r, c_r, h_r
-
-
 def symmetric_5_sided_mesh(W, lt_, lr_, bang_, hfactor, wfactor):
     """
     Create a symmetric, 5-sided wing mesh
