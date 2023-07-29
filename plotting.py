2ac7e289205cb6abb0e3619a35404d78e7e5be39
senwa105 <senwa105@gmail.com>
2023-07-28 22:07:22 -0400

    add more folders to differentiate plots

--- plotting.py
+++ plotting.py
@@ -5,18 +5,30 @@ def create_directories(base_path):
     os.makedirs(base_path, exist_ok=True)
     base_dir = Path(base_path)
 
-    mesh_dir = base_dir / Path("mesh")
-    if not mesh_dir.exists():
-        mesh_dir.mkdir()
+    mesh2d_dir = base_dir / Path("mesh2d")
+    if not mesh2d_dir.exists():
+        mesh2d_dir.mkdir()
 
-    debug_dir = base_dir / Path("debug")
-    if not debug_dir.exists():
-        debug_dir.mkdir()
+    mesh3d_dir = base_dir / Path("mesh3d")
+    if not mesh3d_dir.exists():
+        mesh3d_dir.mkdir()
+
+    airfoil_vel_dir = base_dir / Path("airfoil_vel")
+    if not airfoil_vel_dir.exists():
+        airfoil_vel_dir.mkdir()
+
+    GAMA_dir = base_dir / Path("GAMA")
+    if not GAMA_dir.exists():
+        GAMA_dir.mkdir()
 
     wake_dir = base_dir / Path("wake")
     if not wake_dir.exists():
         wake_dir.mkdir()
 
-    f_and_m = base_dir / Path("f&m")
-    if not f_and_m.exists():
-        f_and_m.mkdir()
+    force_dir = base_dir / Path("force")
+    if not force_dir.exists():
+        force_dir.mkdir()
+
+    moment_dir = base_dir / Path("moment")
+    if not moment_dir.exists():
+        moment_dir.mkdir()
