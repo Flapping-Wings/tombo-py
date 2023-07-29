a20ee0fdc3c335757673438ad0f6f91838814a8d
senwa105 <senwa105@gmail.com>
2023-07-28 22:13:09 -0400

    add plotting_funcs dict

--- plotting.py
+++ plotting.py
@@ -1,34 +1,25 @@
 import os
 from pathlib import Path
 
+def dummy():
+    pass
+
+# Relate each plot type to its corresponding function
+plotting_funcs = {
+    'mesh2d': dummy,
+    'mesh3d': dummy,
+    'airfoil_vel': dummy,
+    'GAMA': dummy,
+    'wake': dummy,
+    'force': dummy,
+    'moment': dummy
+}
+
 def create_directories(base_path):
     os.makedirs(base_path, exist_ok=True)
     base_dir = Path(base_path)
 
-    mesh2d_dir = base_dir / Path("mesh2d")
-    if not mesh2d_dir.exists():
-        mesh2d_dir.mkdir()
-
-    mesh3d_dir = base_dir / Path("mesh3d")
-    if not mesh3d_dir.exists():
-        mesh3d_dir.mkdir()
-
-    airfoil_vel_dir = base_dir / Path("airfoil_vel")
-    if not airfoil_vel_dir.exists():
-        airfoil_vel_dir.mkdir()
-
-    GAMA_dir = base_dir / Path("GAMA")
-    if not GAMA_dir.exists():
-        GAMA_dir.mkdir()
-
-    wake_dir = base_dir / Path("wake")
-    if not wake_dir.exists():
-        wake_dir.mkdir()
-
-    force_dir = base_dir / Path("force")
-    if not force_dir.exists():
-        force_dir.mkdir()
-
-    moment_dir = base_dir / Path("moment")
-    if not moment_dir.exists():
-        moment_dir.mkdir()
+    for key in plotting_funcs.keys():
+        dir = base_dir / Path(key)
+        if not dir.exists():
+            dir.mkdir()
